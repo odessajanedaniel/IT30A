8ff4ea3741c297401941ec4167480b2ef1e3da99
odessajanedaniel <dessangdaniel@gmail.com>
2026-08-26 15:46:37 +0800

    added all activity

--- backup/2026_08_26_15_40_58_library_db.sql
+++ backup/2026_08_26_15_40_58_library_db.sql
@@ -24,6 +24,33 @@ CREATE DATABASE /*!32312 IF NOT EXISTS*/ `library_db` /*!40100 DEFAULT CHARACTER
 USE `library_db`;
 
 --
+-- Table structure for table `books`
+--
+
+DROP TABLE IF EXISTS `books`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `books` (
+  `book_id` int(11) NOT NULL AUTO_INCREMENT,
+  `book_title` varchar(50) NOT NULL,
+  `book_author` varchar(50) NOT NULL,
+  `book_category` varchar(50) NOT NULL,
+  `book_created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`book_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `books`
+--
+
+LOCK TABLES `books` WRITE;
+/*!40000 ALTER TABLE `books` DISABLE KEYS */;
+INSERT INTO `books` VALUES (1,'The Great Gatsby','F. Scott Fitzgerald','Fiction','2026-08-26 07:32:13'),(2,'Pride and Prejudice','Jane Austen','Romance','2026-08-26 07:32:13'),(3,'Animal Farm','George Orwell','Satire','2026-08-26 07:32:13');
+/*!40000 ALTER TABLE `books` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `students`
 --
 
@@ -35,8 +62,9 @@ CREATE TABLE `students` (
   `student_first_name` varchar(50) NOT NULL,
   `student_last_name` varchar(50) NOT NULL,
   `student_course` varchar(50) NOT NULL,
+  `student_created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   PRIMARY KEY (`student_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -45,7 +73,7 @@ CREATE TABLE `students` (
 
 LOCK TABLES `students` WRITE;
 /*!40000 ALTER TABLE `students` DISABLE KEYS */;
-INSERT INTO `students` VALUES (1,'ODESSA','DANIEL','BSIT');
+INSERT INTO `students` VALUES (1,'ODESSA','DANIEL','BSIT','2026-08-22 11:14:48'),(2,'ODING','DANIEL','BSIT','2026-08-22 11:14:48'),(3,'JANE','DANIEL','BSIT','2026-08-22 11:17:57'),(4,'MS.JNE','DANIEL','BSIT','2026-08-22 11:18:49');
 /*!40000 ALTER TABLE `students` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -58,4 +86,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2026-08-22 19:04:21
+-- Dump completed on 2026-08-26 15:41:01
