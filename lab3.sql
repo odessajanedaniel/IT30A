f7b8647285a28c8746175c2f13ad7c530fbd8076
odessajanedanniel <dessangdaniel@gmail.com>
2026-08-29 17:56:42 +0800

    added borrowed_date_time

--- lab3.sql
+++ lab3.sql
@@ -35,20 +35,18 @@ INSERT INTO borrow (student_id,book_id) VALUES
 
 SELECT
    br.borrow_id,
-
-   s.student_id,
+   
    CONCAT(
      s.student_first_name,
      ' ',
      s.student_last_name
    ) as student_name,
-   s.student_course,
-
+  
    b.book_title,
    b.book_author,
    b.book_category,
-
-   br.borrow_date
+   br.borrow_date,
+   br.borrow_return_date
 FROM borrow br
 JOIN students s ON
     br.student_id - s.student_id
@@ -57,3 +55,33 @@ JOIN books b ON
 
 WHERE br.borrow_return_date is NULL
 ORDER BY br.borrow_date  DESC;
+
+--august 27
+
+SELECT
+    br.borrow_id,
+    
+    CONCAT(
+        s.student_first_name,
+        ' ',
+        s.student_last_name
+    ) AS student_name,
+   
+    b.book_title,
+    b.book_author,
+    b.book_category,
+
+    br.borrow_date,
+    br.borrow_return_date
+FROM borrow br
+JOIN students s
+    ON br.student_id = s.student_id
+JOIN books b
+    ON br.book_id = b.book_id
+WHERE br.borrow_return_date is NOT NULL;
+ORDER BY br.borrow_date DESC;
+
+UPDATE borrow
+SET borrow_return_date = CURRENT_TIMESTAMP
+-- Set borrow id for the borrowed you want to return
+WHERE borrow_id = 1 AND borrow_return_date IS NULL;
--- readme.txt
+++ readme.txt
@@ -11,8 +11,7 @@ INSERT INTO <table_name_in_plural> ();
 Utility Commands
 \! clssss
 mysqldump -u root -p --databases library_db >C:\dev2\it30a\backup\08182026_library_db.mysql
-
-mysqldump -u root -p --database library_db > "C:\dev2\it30a\backup\%date:~-4%_%date:~4,2%_%date:~7,2%_%time:~0,2%_%time:~3,2%_%time:~6,2%_%library_db.sql"
+mysqldump -u root -p --databases library_db > "C:\dev2\it30a\backup\%date:~6,4%-%date:~3,2%-%date:~0,2%_%time:~0,2%-%time:~3,2%-%time:~6,2%_library_db.sql"
 ALTER TABLE students ADD COLUMN student_cretaed_at TIMESTAMP NULL DEFAULT NULL;
 UPDATE students SET student_cretaed_at = CURRENT_TIMESTAMP WHERE student_created_at IS NULL;
 ALTER TABLE students MODIFY COLUMN student_created+at TIMESTAMP NOT  NULL DEFAULT CURRENT_TIMESTAMP;
